032217968a6488d8c1a54b9f895c9393d9993098
Ciprian Turbatu <ciprian@betaroot.app>
2020-08-23 14:23:36 +0300

    add GetPlatformApp draft

--- lib/route_manager.dart
+++ lib/route_manager.dart
@@ -4,6 +4,7 @@ export 'src/navigation/routes/get_route.dart';
 export 'src/navigation/routes/default_route.dart';
 export 'src/navigation/routes/observers/route_observer.dart';
 export 'src/navigation/root/root_widget.dart';
+export 'src/navigation/root/root_platform_widget.dart';
 export 'src/navigation/snackbar/snack_route.dart';
 export 'src/navigation/bottomsheet/bottomsheet.dart';
 export 'src/navigation/snackbar/snack.dart';
